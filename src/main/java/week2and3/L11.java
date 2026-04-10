568c24c18f622882fd06ffc86c6e835097a22dc0
OM <30raiom@gmail.com>
2026-04-10 09:10:01 +0530

    weeklyassignment

---
+++ src/main/java/week2and3/L11.java
@@ -0,0 +1,11 @@
+package week2and3;
+
+import java.util.Scanner;
+
+public class L11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println("Is the number " + number + " divisible by 5? " + (number % 5 == 0));
+    }
+}
